e556717658697039c011d000f3b6548009be823a
AniketRamteke <aniket.ramteke09@gmail.com>
2024-06-03 15:54:36 +0530

    write file

--- routes/webhook.mjs
+++ routes/webhook.mjs
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import fs from "fs";
+
 const webhookRouter = Router();
 
 webhookRouter.post("/", async (req, res) => {
   res.send({ webhookData: req.headers });
+  fs.writeFileSync("webhookData.json", JSON.stringify(req.headers));
 });
 
 export { webhookRouter };
